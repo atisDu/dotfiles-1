637c7bfc100d0b0260d6690758f36dfecacbd16e
Sujay R <68642484+sujay1844@users.noreply.github.com>
2021-04-13 07:40:34 +0530

    My dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+function e
+	exit $argv
+end
+function ls
+	exa -lah $argv
+end
+function l
+	exa $argv
+end
+function cat
+	bat $argv
+end
+function find
+	fd $argv
+end
+function yay
+	paru $argv
+end
---
+++ qtile-back/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/home/sujay1844/.config/polybar/launch.sh
+nitrogen --restore &
+blueman-applet &
+telegram-desktop
