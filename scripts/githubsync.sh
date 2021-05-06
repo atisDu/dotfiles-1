066b2adab9d17e5914df39de66bef7c4e280e218
sujay1844 <sujay1844@protonmail.com>
2021-05-06 18:41:13 +0530

    Update as of 06/05/21-18:41:13

--- scripts/githubsync.sh
+++ scripts/githubsync.sh
@@ -2,14 +2,16 @@
 
 echo "Changing directory to ~/Github/dotfiles..."
 cd ~/Github/dotfiles/
+
+# Here we are parse the names of the folders in the current directory into an array: list[]
 i=0
 while read line
 do
     list[$i]="$line"        
     (( i++ ))
 done < <(ls ~/Github/dotfiles)
-#list=("awesome" "kitty" "polybar.old" "ranger" "sway" "waybar" "README.md"  "awesome-back" "leftwm" "qtile" "rofi" "terminator" "alacritty" "fish" "polybar" "qtile-back" "scripts" "termite" "hi")
 
+# Now to update the contents of directories in the Github repo, we remove, copy a new one from ~/.config and then add it to git
 echo "List of Directories being synced..."
 for ((i=0 ; i < ${#list[@]} ; i++)); do
 	echo "Removing ${list[i]}"
@@ -20,6 +22,8 @@ for ((i=0 ; i < ${#list[@]} ; i++)); do
 	git add "${list[i]}"
 done
 echo "Sync complete..."
+
+# Then we commit these changes with a message carrying the current date and time. And finally, we push these new directories to the remote Github repo
 echo "Committing changes..."
 git commit -m "Update as of $(date +%d/%m/%y-%T)"
 echo "Pushing to remote Github repository (if any)..."
