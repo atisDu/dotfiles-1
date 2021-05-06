6ef7b466d28efd23e905b780cb2987f1d0e7bcae
sujay1844 <sujay1844@protonmail.com>
2021-05-06 17:01:00 +0530

    Update as of 06/05/21-17:01:00

--- scripts/githubsync.sh
+++ scripts/githubsync.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cd ~/Github/dotfiles
-list="$(ls ~/Github/dotfiles)"
-rm -rf ~/Github/dotfiles/*
-cp -r ~/.config/$list .
-git add .
-git commit -m "Update as of $(date +%d/%m/%y)"
+echo "Changing directory to ~/Github.dotfiles..."
+cd ~/.config/
+list=("awesome" "kitty" "polybar.old" "ranger" "sway" "waybar" "README.md"  "awesome-back" "leftwm" "qtile" "rofi" "terminator" "alacritty" "fish" "polybar" "qtile-back" "scripts" "termite" "hi")
+echo "List of Directories being synced..."
+for ((i=0 ; i <= ${#list[@]} ; i++)); do
+	echo ${list[i]}
+	git add "${list[i]}"
+done
+echo "Sync complete..."
+echo "Adding to git..."
+#git add .
+echo "Committing changes..."
+git commit -m "Update as of $(date +%d/%m/%y-%T)"
+echo "Pushing to remote Github repository..."
 git push
--- scripts/gitsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/Github/dotfiles
-cp -r ~/.config/$(/bin/ls ~/Github/dotfiles/) .
-git add .
-git commit -m "Update as of $(date +%d/%m/%y)"
-git push
