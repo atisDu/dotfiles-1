cfe9eb6b24b2b0c7aea5a4c3ff5f7aa3ef5457e9
sujay1844 <sujay1844@protonmail.com>
2021-05-06 18:28:15 +0530

    Update as of 06/05/21-18:28:15

--- scripts/githubsync.sh
+++ scripts/githubsync.sh
@@ -10,24 +10,14 @@ do
 done < <(ls ~/Github/dotfiles)
 #list=("awesome" "kitty" "polybar.old" "ranger" "sway" "waybar" "README.md"  "awesome-back" "leftwm" "qtile" "rofi" "terminator" "alacritty" "fish" "polybar" "qtile-back" "scripts" "termite" "hi")
 
-trim() {
-    local var="$*"
-    # remove leading whitespace characters
-    var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"   
-    printf '%s' "$var"
-}
-
 echo "List of Directories being synced..."
 for ((i=0 ; i < ${#list[@]} ; i++)); do
-	trim ${list[i]}
 	echo "Removing ${list[i]}"
-	rm -r ~/Github/dotfiles/"${list[i]}"/
+	rm -r ~/Github/dotfiles/"${list[i]}"
 	echo "Copying ${list[i]}"
-	cp -r ~/.config/"${list[i]}"/ ~/Github/dotfiles
+	cp -r ~/.config/"${list[i]}" ~/Github/dotfiles
 	echo "Adding ${list[i]} to git"
-	git add "${list[i]}"/
+	git add "${list[i]}"
 done
 echo "Sync complete..."
 echo "Committing changes..."
