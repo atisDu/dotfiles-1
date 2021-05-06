b788d2d378a92abad00448703afb22b42e8d9af6
sujay1844 <sujay1844@protonmail.com>
2021-05-06 17:15:05 +0530

    Update as of 06/05/21-17:15:05

--- scripts/githubsync.sh
+++ scripts/githubsync.sh
@@ -4,9 +4,12 @@ echo "Changing directory to ~/Github/dotfiles..."
 cd ~/Github/dotfiles/
 list=("awesome" "kitty" "polybar.old" "ranger" "sway" "waybar" "README.md"  "awesome-back" "leftwm" "qtile" "rofi" "terminator" "alacritty" "fish" "polybar" "qtile-back" "scripts" "termite" "hi")
 echo "List of Directories being synced..."
-for ((i=0 ; i <= ${#list[@]} ; i++)); do
-	echo ${list[i]}
+for ((i=0 ; i < ${#list[@]} ; i++)); do
+	echo "Removing ${list[i]}"
+	rm -r ~/Github/dotfiles/"${list[i]}"
+	echo "Copying ${list[i]}"
 	cp -r ~/.config/"${list[i]}" ~/Github/dotfiles
+	echo "Adding ${list[i]} to git"
 	git add "${list[i]}"
 done
 echo "Sync complete..."
